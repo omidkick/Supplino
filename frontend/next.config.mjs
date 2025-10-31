ccc2e35e042dd7d60e3823d19f5cd529d16ebe79
Omid Jabbari <omidjabbri0041@gmail.com>
2025-10-31 10:20:26 +0330

    fix: update next.config.mjs for deployment and adjust build script

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,31 +1,31 @@
-// ! for deploying :
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api.supplino.ir",
-        pathname: "/uploads/**",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+//! for deploying :
 
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   images: {
 //     remotePatterns: [
 //       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "5000",
-//    pathname: "/**",      },
+//         protocol: "https",
+//         hostname: "api.supplino.ir",
+//         pathname: "/uploads/**",
+//       },
 //     ],
 //   },
 // };
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+   pathname: "/**",      },
+    ],
+  },
+};
+
+export default nextConfig;
